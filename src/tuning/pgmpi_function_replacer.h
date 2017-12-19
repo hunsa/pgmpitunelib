762e5c3a41be9160b84de0b3399be7eb845f2c4f
Alexandra Carpen-Amarie <carpenamarie@par.tuwien.ac.at>
2017-12-19 13:14:12 +0100

    Added Licence, Readme and copyright headers

--- src/tuning/pgmpi_function_replacer.h
+++ src/tuning/pgmpi_function_replacer.h
@@ -1,9 +1,28 @@
-/*
- * pgmpi_function_replacer.h
- *
- *  Created on: Mar 2, 2017
- *      Author: sascha
+/*  PGMPITuneLib - Library for Autotuning MPI Collectives using Performance Guidelines
+ *  
+ *  Copyright 2017 Sascha Hunold, Alexandra Carpen-Amarie
+ *      Research Group for Parallel Computing
+ *      Faculty of Informatics
+ *      Vienna University of Technology, Austria
+ *  
+ *  <license>
+ *      This library is free software; you can redistribute it
+ *      and/or modify it under the terms of the GNU Lesser General Public
+ *      License as published by the Free Software Foundation; either
+ *      version 2.1 of the License, or (at your option) any later version.
+ *  
+ *      This library is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *      Lesser General Public License for more details.
+ *  
+ *      You should have received a copy of the GNU Lesser General Public
+ *      License along with this library; if not, write to the Free
+ *      Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *      Boston, MA 02110-1301 USA
+ *  </license>
  */
+
 
 #ifndef SRC_TUNING_PGMPI_FUNCTION_REPLACER_H_
 #define SRC_TUNING_PGMPI_FUNCTION_REPLACER_H_
--- src/util/keyvalue_store.h
+++ src/util/keyvalue_store.h
@@ -1,25 +1,28 @@
-/*  ReproMPI Benchmark
- *
- *  Copyright 2015 Alexandra Carpen-Amarie, Sascha Hunold
-    Research Group for Parallel Computing
-    Faculty of Informatics
-    Vienna University of Technology, Austria
+/*  PGMPITuneLib - Library for Autotuning MPI Collectives using Performance Guidelines
+ *  
+ *  Copyright 2017 Sascha Hunold, Alexandra Carpen-Amarie
+ *      Research Group for Parallel Computing
+ *      Faculty of Informatics
+ *      Vienna University of Technology, Austria
+ *  
+ *  <license>
+ *      This library is free software; you can redistribute it
+ *      and/or modify it under the terms of the GNU Lesser General Public
+ *      License as published by the Free Software Foundation; either
+ *      version 2.1 of the License, or (at your option) any later version.
+ *  
+ *      This library is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *      Lesser General Public License for more details.
+ *  
+ *      You should have received a copy of the GNU Lesser General Public
+ *      License along with this library; if not, write to the Free
+ *      Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *      Boston, MA 02110-1301 USA
+ *  </license>
+ */
 
-<license>
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-</license>
-*/
 
 #ifndef PGMPI_KEYVALUE_STORE_H_
 #define PGMPI_KEYVALUE_STORE_H_
